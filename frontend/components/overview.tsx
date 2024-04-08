053f7583a86d992c7671fa8d65a7965377d186a7
Frank <frank.pfeift@protonmail.com>
2024-04-08 17:45:19 +0200

    style: changed container location

--- frontend/components/overview.tsx
+++ frontend/components/overview.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import Connect from "../public/connect-icon.svg";
 import Switchboard from "../public/switchboard-icon.svg";
+import Link from "next/link";
 export default function Overview({ name }: { name: string }) {
     return (
-        <div className="flex flex-col gap-4 max-w-xl items-start ">
+        <div className="flex flex-col gap-4 max-w-xl items-start px-12">
             <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href="#">
-                        <span className="absolute inset-0"></span>
-                        {name}
-                    </a>
+                    <span className="absolute inset-0"></span>
+                    {name}
                 </h3>
             </div>
             <div className="relative flex flex-col ">
@@ -17,10 +16,10 @@ export default function Overview({ name }: { name: string }) {
                     <Image width="32" height="32" alt="MakerDAO" src={Connect} className="h-10 w-10 rounded-full bg-gray-50" />
                     <div className="text-sm leading-6">
                         <p className="font-semibold text-gray-900">
-                            <a href={`https://apps.powerhouse.io/${name.toLocaleLowerCase()}/connect`}>
+                            <Link href={`https://apps.powerhouse.io/${name.toLocaleLowerCase()}/connect`} target="_blank">
                                 <span className="absolute inset-0"></span>
                                 Connect
-                            </a>
+                            </Link>
                         </p>
                     </div>
                 </div>
@@ -28,10 +27,10 @@ export default function Overview({ name }: { name: string }) {
                     <Image width="32" height="32" alt="MakerDAO" src={Switchboard} className="h-10 w-10 rounded-full bg-gray-50" />
                     <div className="text-sm leading-6">
                         <p className="font-semibold text-gray-900">
-                            <a href={`https://apps.powerhouse.io/${name.toLocaleLowerCase()}/switchboard`}>
+                            <Link href={`https://apps.powerhouse.io/${name.toLocaleLowerCase()}/switchboard`} target="_blank">
                                 <span className="absolute inset-0"></span>
                                 Switchboard
-                            </a>
+                            </Link>
                         </p>
                     </div>
                 </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,34 +1,34 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import Overview from "../components/overview";
 import Powerhouse from "../public/powerhouse-icon-colour.svg";
 import Renown from "../public/renown-icon.svg";
 import Fusion from "../public/fusion-icon.png";
 import Academy from "../public/academy-icon.svg";
+import Link from "next/link";
 const Home: NextPage = () => {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="bg-white py-24 sm:py-32">
-                <div className="mx-auto max-w-4xl  flex flex-col gap-8">
+                <div className="mx-auto max-w-4xl   flex flex-col gap-8">
                     <div className="mx-auto max-w-2xl lg:mx-0 flex flex-row gap-4">
                         <Image src={Powerhouse} alt="Powerhouse" width="40" height="40" />
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Apps of Powerhouse</h2>
                         {/* <p className="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p> */}
                     </div>
-                    <div className="grid grid-cols-3 gap-x-8">
-                        <div className="relative flex items-center gap-x-4">
+                    <div className="grid grid-cols-3 gap-x-8 ">
+                        <div className="relative flex items-center gap-x-4 px-12">
                             <Image width="32" height="32" alt="MakerDAO" src={Renown} className="h-10 w-10 rounded-full bg-gray-50" />
                             <div className="text-sm leading-6">
                                 <p className="font-semibold text-gray-900">
-                                    <a href={`https://apps.powerhouse.io/renown`}>
+                                    <Link href={`https://apps.powerhouse.io/renown`} target="_blank">
                                         <span className="absolute inset-0"></span>
                                         Renown
-                                    </a>
+                                    </Link>
                                 </p>
                             </div>
                         </div>
-                        <div className="relative flex items-center gap-x-4">
+                        <div className="relative flex items-center gap-x-4 px-12">
                             <Image width="32" height="32" alt="MakerDAO" src={Academy} className="h-10 w-10 rounded-full bg-gray-50" />
                             <div className="text-sm leading-6">
                                 <p className="font-semibold text-gray-900">
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="relative flex items-center gap-x-4">
+                        <div className="relative flex items-center gap-x-4 px-12">
                             <Image width="32" height="32" alt="MakerDAO" src={Fusion} className="h-10 w-10 rounded-full bg-gray-50" />
                             <div className="text-sm leading-6">
                                 <p className="font-semibold text-gray-900">
