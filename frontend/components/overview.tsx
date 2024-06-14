1ba56c68daf017773b4c1c763a53252056a67894
Frank <frank.pfeift@protonmail.com>
2024-06-14 10:31:16 +0200

    feat: added dev env

--- frontend/components/overview.tsx
+++ frontend/components/overview.tsx
@@ -69,6 +69,37 @@ export default function Overview({ name }: { name: string }) {
                     </div>
                 </div>
             </div>
+            {name === "Powerhouse" && (
+                <div className="flex flex-row">
+                    <div className="flex flex-0  w-3">
+                        <h2 className="text-base font-semibold text-gray-900 -rotate-90">Dev</h2>
+                    </div>
+                    <div className="flex flex-col grow flex-1">
+                        <div className="relative flex items-center gap-x-4">
+                            <Image width="32" height="32" alt="MakerDAO" src={Connect} className="h-10 w-10 rounded-full bg-gray-50" />
+                            <div className="text-sm">
+                                <p className="font-semibold text-gray-900">
+                                    <Link href={`https://apps.powerhouse.io/develop/${name.toLocaleLowerCase()}/connect/`} target="_blank">
+                                        <span className="absolute inset-0"></span>
+                                        Connect
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="relative flex items-center gap-x-4">
+                            <Image width="32" height="32" alt="MakerDAO" src={Switchboard} className="h-10 w-10 rounded-full bg-gray-50" />
+                            <div className="text-sm">
+                                <p className="font-semibold text-gray-900">
+                                    <Link href={`https://apps.powerhouse.io/develop/${name.toLocaleLowerCase()}/switchboard`} target="_blank">
+                                        <span className="absolute inset-0"></span>
+                                        Switchboard
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
